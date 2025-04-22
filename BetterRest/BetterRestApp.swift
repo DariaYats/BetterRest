07a9b30281e74a03f38f9a5557ec63e0c94a6a5f
Daria Yats <dariayatsyniuk@gmail.com>
2025-04-22 16:25:52 +0200

    stage whole project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Дарья Яцынюк on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
