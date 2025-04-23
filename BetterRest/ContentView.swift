900532e24ac816c0ba24d647597ec6521189d645
Daria Yats <dariayatsyniuk@gmail.com>
2025-04-23 13:17:23 +0200

    remove calculating button

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,63 +11,56 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    
+
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components) ?? Date.now
     }
-    
+
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
-    
+
     var body: some View {
         NavigationView {
             Form {
-                //VStack(alignment: .leading, spacing: 0) {
                 Section {
                     Text("When do you want to wake up?")
                         .font(.headline)
-                    
+
                     DatePicker("Pleas enter a day", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                 }
-                
-                //VStack(alignment: .leading, spacing: 0) {
+
                 Section {
-                Text("Desired amount of sleep")
+                    Text("Desired amount of sleep")
                         .font(.headline)
-                    
+
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
-                
-                //VStack(alignment: .leading, spacing: 0) {
+
                 Section {
                     Text("Daily coffee intake")
                         .font(.headline)
-                    
+
                     Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                 }
-                
-              
+
+                Section {
+                    Text(calculateBedtime())
+                        .font(.title2)
+                        .fontWeight(.medium)
+                }
             }
             .navigationTitle("BetterRest")
-               
-            
-        }
-        
-        .alert(alertTitle, isPresented: $showingAlert) {
-            Button("OK") { }
-        } message: {
-            Text(alertMessage)
         }
     }
-    
-    
-    
-    func calculateBedtime() {
+
+
+
+    func calculateBedtime() -> String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -77,13 +70,11 @@ struct ContentView: View {
             let minute = (components.minute ?? 0) * 60
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            let formattedSleepTime = sleepTime.formatted(date: .omitted, time: .shortened)
+            return "Your ideal bedtime is \(formattedSleepTime)"
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Sorry, there was a problem calculating your bedtime."
         }
-        showingAlert = true
     }
 }
 
